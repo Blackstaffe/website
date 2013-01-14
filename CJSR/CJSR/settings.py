2d734e154f9d3bae83233e75995f3a856152e08c
Mitchell <blackstaffe.mitchell@gmail.com>
2013-01-14 16:24:29 -0700

    added admin and showmanger tables

--- CJSR/CJSR/settings.py
+++ CJSR/CJSR/settings.py
@@ -115,8 +115,9 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
+    # Showdb manager
+    'showmanager'
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
