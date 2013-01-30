acee0ab2c36086a8087f95a2140ce10164f7bd31
Mitchell <blackstaffe.mitchell@gmail.com>
2013-01-29 22:51:32 -0700

    added some related tables to database structure

--- CJSR/showmanager/admin.py
+++ CJSR/showmanager/admin.py
@@ -1,9 +1,31 @@
 from django.contrib import admin
-from showmanager.models import Show
+from showmanager.models import Show, Hosts, Contact
+from django.contrib.admin import SimpleListFilter
+class DayListFilter(SimpleListFilter):
+    title = ('Day')
+    parameter_name = 'day'
+    
+class TimeListFilter(SimpleListFilter):
+    title = ('TimeSlot')
+    parameter_name = 'starttime'
+
+class HostsInline(admin.StackedInline):
+#    inlines = [ContactInline]
+# Cannot stack menus inline, need to find work around (seperate hosts 
+# interaction perhaps
+    model = Hosts
+    extra = 1
+    
+class ContactInline(admin.StackedInline):
+    model = Contact
+    extra = 1
 
 class ShowAdmin(admin.ModelAdmin):
     fieldsets = [
-        ('Basics', {'fields': ['name', 'day', 'starttime', 'stoptime', 'hosts', 'tagline', 'description',]}),
-        ('Contact', {'fields': ['website', 'email', 'twitter', 'facebook']}),
+        ('Basics', {'fields': ['name', 'day', 'starttime', 'tagline', 'description',]}),
     ]
+    inlines = [HostsInline]
+    list_display = ('name', 'day', 'starttime')
+#   list_filter = (DayListFilter, TimeListFilter)
+    search_fields = ['name', 'hosts',] 
 admin.site.register(Show, ShowAdmin)
--- CJSR/showmanager/models.py
+++ CJSR/showmanager/models.py
@@ -12,13 +12,24 @@ class Show(models.Model):
         ('Friday', 'Friday'), 
         ('Saturday','Saturday')])
     starttime = models.TimeField()
-    stoptime = models.TimeField()
-    hosts = models.CharField(max_length=100)
+    length = models.TimeField() # TODO > add choices
     tagline = models.TextField(max_length=200)
     description = models.TextField(max_length=2000)
-    website = models.URLField()
-    email = models.EmailField(max_length=254)
-    twitter = models.CharField(max_length=15)
-    facebook = models.URLField()
     def __unicode__(self):
         return self.name    
+
+class Hosts(models.Model):
+    show = models.ForeignKey(Show)
+    hosts = models.CharField(max_length=200)
+    def __unicode__(self):
+        return self.name
+
+class Contact(models.Model):
+    host = models.ForeignKey(Hosts)
+    website = models.URLField(blank=True, null=True)
+    email = models.EmailField(max_length=254, blank=True, null=True)
+    twitter = models.CharField(max_length=15, blank=True, null=True)
+    facebook = models.URLField(blank=True, null=True)
+    def __unicode__(self):
+        return self.name
+
