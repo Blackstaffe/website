dacf702eae8e3e6424612f06029840754a6f1ae4
Mitchell <blackstaffe.mitchell@gmail.com>
2013-02-07 19:19:15 -0700

    db models and admin interface updated

--- CJSR/showmanager/admin.py
+++ CJSR/showmanager/admin.py
@@ -1,25 +1,25 @@
 from django.contrib import admin
 from showmanager.models import Show, Host
 from django.contrib.admin import SimpleListFilter
+
+"""
 class DayListFilter(SimpleListFilter):
     title = ('Day')
     parameter_name = 'day'
-    
 class TimeListFilter(SimpleListFilter):
-    title = ('TimeSlot')
+    title = ('Time Slot')
     parameter_name = 'starttime'
-
+"""
 class HostInline(admin.StackedInline):
     model = Host
     extra = 1
-
 class ShowAdmin(admin.ModelAdmin):
     fieldsets = [
-        ('Basics', {'fields': ['name', 'day', 'starttime', 'tagline', 'description',]}), 
+        ('Basics', {'fields': ['name', 'day', 'starttime', 'length', 'genre', 'description', 'inactive']}), 
         ('Contact Info', {'fields': ['website', 'email', 'twitter', 'facebook']}),
     ]
     inlines = [HostInline]
     list_display = ('name', 'day', 'starttime')
-#   list_filter = (DayListFilter, TimeListFilter)
+    list_filter = ['starttime', 'day']
     search_fields = ['name', 'host',] 
 admin.site.register(Show, ShowAdmin)
--- CJSR/showmanager/models.py
+++ CJSR/showmanager/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 # Show database model
 class Show(models.Model):
+    # Basic Info 
     name = models.CharField(max_length=20)
-    day = models.CharField(max_length=10, choices=[(
-        'Sunday', 'Sunday'),
+    day = models.CharField(max_length=10, choices=[
+        ('Sunday', 'Sunday'),
         ('Monday', 'Monday'),
         ('Tuesday', 'Tuesday'),
         ('Wednesday', 'Wednesday'), 
@@ -12,9 +13,31 @@ class Show(models.Model):
         ('Friday', 'Friday'), 
         ('Saturday','Saturday')])
     starttime = models.TimeField()
-    length = models.TimeField() # TODO > add choices
-    tagline = models.TextField(max_length=200)
+    # This is a tad weird in tha admin pannel because it doesn't seem to 
+    # show up but it seems to enter into the database fine, could have to do
+    # with the IntegerField, will look into, could also be stored as a 
+    # CharField but that would require converting it back to an integer after.
+    # better to figure out, and I need to check if it is being inserted into
+    # database. 
+    length = models.IntegerField(choices=[
+        (30, 'Half-hour'),
+        (60, 'Hour'),
+        (90, 'Hour and a half'),
+        (120, 'Two hours' ),
+        (150, 'Two and a half hours'),
+        (180, 'Three hours')])
+    genre = models.CharField(max_length=15, choices=[
+        ('Eclectic', 'Eclectic'),
+        ('Spoken Word', 'Spoken Word'),
+        ('News', 'News'),
+        ('Roots', 'Roots'),
+        ('Hip-Hop', 'Hip-Hop'),
+        ('Cultural', 'Cultural'),
+        ('Loud', 'Loud'),
+        ('Specialty', 'Specialty')])
     description = models.TextField(max_length=2000)
+    inactive = models.BooleanField()
+    # Contact Info 
     website = models.URLField(blank=True, null=True)
     email = models.EmailField(max_length=254, blank=True, null=True)
     twitter = models.CharField(max_length=15, blank=True, null=True)
@@ -26,4 +49,4 @@ class Host(models.Model):
     show = models.ForeignKey(Show)
     host = models.CharField(max_length=200)
     def __unicode__(self):
-        return self.name
+        return self.host
