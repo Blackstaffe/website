62c1c62377369760428db7b3b995ac96f1a6edb4
Mitchell Blackstaffe <blackstaffe.mitchell@gmail.com>
2013-02-27 02:07:21 -0700

    added some template stuff to show function

--- CJSR/archive/views.py
+++ CJSR/archive/views.py
@@ -1,10 +1,18 @@
 from django.http import HttpResponse
+from django.template import Context, loader
+
+from showmanager.models import Show, Host
 # Create your views here.
 def index(request):
-    return HttpResponse("Hiya I'm a test")
+    return HttpResponse("Some things would go here")
 
 def show_detail(request, show_id):
-    return HttpResponse("Show %s" % show_id)
+    return HttpResponse("This is where details about a show would go")
 
 def shows(request):
-    return HttpResponse("This is where a list of shows would go")
+    latest_logs = Show.objects.order_by('timeslot')[:12] #this will break when the db model is updated
+    template = loader.get_template('archive/index.html')
+    context = Context({
+        'latest_logs': latest_logs,
+    })
+    return HttpResponse(template.render(context))
