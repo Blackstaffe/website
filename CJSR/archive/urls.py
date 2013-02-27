77dfcfe1fc39cea6269809fc355e7e62047c4758
Mitchell Blackstaffe <blackstaffe.mitchell@gmail.com>
2013-02-27 00:30:10 -0700

    added some boilerplate views for the index and show pages of the archive

--- CJSR/archive/urls.py
+++ CJSR/archive/urls.py
@@ -4,10 +4,13 @@ from archive import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-#    url(r'^archive/shows', views.shows),
-#    url(r'^archive/shows/showID', include(logmachine.views.show)),
-#    url(r'^archive/genre', include(logmachine.views.genre)),
-#    url(r'^archive/hosts', include(logmachine.views.hosts)),
-#    url(r'^archive/hosts/name', include(logmachine.views.host)),
-#    url(r'^archive/timeslot/', include(logmachine.views.timeslot)),
+    url(r'^shows/$', views.shows),
+    # Url Scheme for show page
+    url(r'^shows/(?P<show_id>\d+)/$', 
+    views.show_detail, 
+    name='show_detail'),
+#    url(r'^/genre/$', views.genre),
+#    url(r'^/hosts', include(logmachine.views.hosts)),
+#    url(r'^/hosts/name', include(logmachine.views.host)),
+#    url(r'^/timeslot/', include(logmachine.views.timeslot)),
 )
--- CJSR/archive/views.py
+++ CJSR/archive/views.py
@@ -1,1 +1,10 @@
+from django.http import HttpResponse
 # Create your views here.
+def index(request):
+    return HttpResponse("Hiya I'm a test")
+
+def show_detail(request, show_id):
+    return HttpResponse("Show %s" % show_id)
+
+def shows(request):
+    return HttpResponse("This is where a list of shows would go")
