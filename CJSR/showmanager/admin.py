f71b9f9873a4e8677aeb4b83cda37b2109f2504c
Mitchell Blackstaffe <blackstaffe.mitchell@gmail.com>
2013-03-01 03:58:55 -0700

    changed inline structure

--- CJSR/showmanager/admin.py
+++ CJSR/showmanager/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from showmanager.models import Show, Host, Genre
 from django.contrib.admin import SimpleListFilter
-
+# TODO switch around the inlines so genre is the parent of shows, hosts 
+# might need to be a many yo many relationship
 """
 class DayListFilter(SimpleListFilter):
     title = ('Day')
@@ -15,6 +16,7 @@ class HostInline(admin.StackedInline):
     extra = 1
 class GenreInline(admin.StackedInline):
     model = Genre
+    max_num = 1
 class GenreAdmin(admin.ModelAdmin):
     model = Genre
     # The model needs to be updated to make genre's addable
@@ -24,9 +26,10 @@ class ShowAdmin(admin.ModelAdmin):
          'description', 'inactive']}),
         ('Contact Info', {'fields': ['website', 'email', 'twitter', 'facebook']}),
     ]
-    inlines = [HostInline, GenreInline,]
+    inlines = [HostInline,]
     list_display = ('name', 'day', 'timeslot')
     list_filter = ['timeslot', 'day', 'genre']
     search_fields = ['name', 'host',] 
+    prepopulated_fields = {'slug':('name',)}
 admin.site.register(Show, ShowAdmin,)
-admin.site.register(Genre)
+admin.site.register(Genre, GenreAdmin)
